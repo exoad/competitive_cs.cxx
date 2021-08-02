4635ac1c0319b952dfd8accf96f5d8193d6fb748
Jack Meng <error666jm@gmail.com>
2021-08-02 07:23:47 -0400

    Create 726div2B.cpp

---
+++ cf/726div2B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int t;
+int main()
+{
+    ll n, m, x, y;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m >> x >> y;
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+}
