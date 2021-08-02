4b1cec702dcfd03c8a6334d0db4dd34bad132f6f
Jack Meng <error666jm@gmail.com>
2021-08-02 07:44:04 -0400

    Create welcometoatcoder.java

---
+++ atcoder/welcometoatcoder.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		String s = sc.next();
+		System.out.println((a + b + c) + " " + s);
+		sc.close();
+	}
+}
