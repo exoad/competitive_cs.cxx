1471d63b80b2bdb8bb4496d9fbf9ef089326e81d
Jack Meng <error666jm@gmail.com>
2021-08-03 08:56:13 -0400

    Create BUYING2.c

---
+++ codechef/BUYING2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n,k,flag=0;
+	    scanf("%d%d",&n,&k);
+	    int arr[n],s=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        scanf("%d",&arr[i]);
+	        s+=arr[i];
+	    }
+	    int sweets=s/k;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(sweets==(s-arr[i])/k)
+	        {
+	            flag=1;
+	            break;
+	        }
+	    }
+	    if(flag==0)
+	    {
+	        printf("%d\n",sweets);
+	    }
+	    else
+	    {
+	        printf("-1\n");
+	    }
+	}
+	return 0;
+}
+
