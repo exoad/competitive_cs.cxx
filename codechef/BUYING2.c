af2e362a381fd28c4f63c81da27ec4f72f3c79fd
Jack Meng <error666jm@gmail.com>
2021-08-03 08:57:23 -0400

    Update BUYING2.c

--- codechef/BUYING2.c
+++ codechef/BUYING2.c
@@ -2,36 +2,31 @@
 
 int main()
 {
-	int t;
-	scanf("%d",&t);
-	while(t--)
-	{
-	    int n,k,flag=0;
-	    scanf("%d%d",&n,&k);
-	    int arr[n],s=0;
-	    for(int i=0;i<n;i++)
-	    {
-	        scanf("%d",&arr[i]);
-	        s+=arr[i];
-	    }
-	    int sweets=s/k;
-	    for(int i=0;i<n;i++)
-	    {
-	        if(sweets==(s-arr[i])/k)
-	        {
-	            flag=1;
-	            break;
-	        }
-	    }
-	    if(flag==0)
-	    {
-	        printf("%d\n",sweets);
-	    }
-	    else
-	    {
-	        printf("-1\n");
-	    }
-	}
-	return 0;
+  int t;
+  scanf("%d", &t);
+  while (t--)
+  {
+    int n, k, flag = 0;
+    scanf("%d%d", &n, &k);
+    int arr[n], s = 0;
+    for (int i = 0; i < n; i++)
+    {
+      scanf("%d", &arr[i]);
+      s += arr[i];
+    }
+    int sweets = s / k;
+    for (int i = 0; i < n; i++)
+    {
+      if (sweets == (s - arr[i]) / k)
+      {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0)
+      printf("%d\n", sweets);
+    else
+      printf("-1\n");
+  }
+  return 0;
 }
-
