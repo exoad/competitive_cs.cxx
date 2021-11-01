53ae757f7742c60e6fdb5c589e74960f092239df
Jack Meng <jackmeng0814@gmail.com>
2021-11-01 14:23:28 -0400

    Update 732div1A.cpp

--- cf/732div1A.cpp
+++ cf/732div1A.cpp
@@ -1,33 +1,35 @@
-#include <bits/stdc++.h>
+//732A
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int T;
-  cin >> T;
-  while (T--)
-  {
-    int N;
-    cin >> N;
-    vector<int> a(N + 1);
-    vector<array<int, 2>> cnt(1e5 + 1);
-    for (int i = 1; i <= N; i++)
-    {
-      cin >> a[i];
-      cnt[a[i]][i % 2]++;
+#define F0R(N) for(int i = 1; i <= N; i++)
+#define WHI(N) while(N--)
+#define MAXIMA 1e+5
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    int T;
+    cin >> T;
+    WHI(T) {
+        int N;
+        cin >> N;
+        vector<int> a(N + 1);
+        vector<array<int, 2>> cnt(MAXIMA + 1);
+        F0R(N) {
+            cin >> a[i];
+            cnt[a[i]][i % 2]++;
+        }
+        sort(a.begin(), a.end());
+        int f = 0;
+        F0R(N) {
+            cnt[a[i]][i % 2]--;
+        }
+        F0R(N) {
+            f |= cnt[a[i]][0] != 0;
+            f |= cnt[a[i]][1] != 0;
+        }
+        cout << (f ? "NO" : "YES") << endl;
     }
-    sort(a.begin(), a.end());
-    int f = 0;
-    for (int i = 1; i <= N; i++)
-      cnt[a[i]][i % 2]--;
-    for (int i = 1; i <= N; i++)
-    {
-      f |= cnt[a[i]][0] != 0;
-      f |= cnt[a[i]][1] != 0;
-    }
-    cout << ((f) ? "NO\n" : "YES\n");
-  }
+    return 0;
 }
