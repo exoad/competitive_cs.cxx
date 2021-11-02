0e7fc4a7a0b6c74e66d991cae7ce14c79ad5f323
Jack Meng <jackmeng0814@gmail.com>
2021-11-02 13:12:17 -0400

    Create 225-distinctstrings.cpp

---
+++ atcoder/225-distinctstrings.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define MAX 26
+using namespace std;
+
+int prima(int n) {
+	int f = 1;
+	for(int i = 2; i <= n; i++) 
+		f *= i;
+	return f;
+}
+
+int solve(string s) {
+	int len = s.length();
+	int f[MAX];
+	memset(f, 0, sizeof(f));
+	for(int i = 0; i < len; i++) 
+		if(s[i] >= 'a')
+			f[s[i] - 'a']++;
+			
+	int fa = 1;
+	for(int i = 0; i < MAX; i++) 
+		fa *= prima(f[i]);
+		
+	return prima(len) / fa;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	cout << solve(s) << endl;
+}
