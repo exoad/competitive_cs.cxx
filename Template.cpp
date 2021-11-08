1caad498b6aabc9eaf79f0b6e63395dc8f0ab0ba
Jack Meng <jackmeng0814@gmail.com>
2021-11-08 13:20:26 -0500

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+using ll = long long;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+	if (sz(name)) {
+		freopen((name + ".in").c_str(), "r", stdin); 
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+int main(void) {}
