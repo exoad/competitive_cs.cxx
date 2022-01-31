688b5eb40d567bfe4b747d77fe278d7718b6acf6
Jack Meng <jackmeng0814@gmail.com>
2022-01-31 13:24:49 -0500

    Update r767div1D2.cpp

---
+++ cf/r767div1D2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+#define pb push_back
+
+using ll = long long;
+using namespace std;
+
+using vll = vector<pair<vector<ll>, ll>>;
+
+const int MOD = 1e9+7;
+
+int main (void) {
+
+}
