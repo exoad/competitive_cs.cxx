bd3a87e977384843d7f6cb3a9eb70e949c1b3217
Jack Meng <jackmeng0814@gmail.com>
2022-03-03 09:20:08 -0500

    Create BOTTOM.cpp

---
+++ codechef/BOTTOM.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define el else if
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int x;
+        cin >> x;
+        cout << 7-x << endl;
+    }
+}
