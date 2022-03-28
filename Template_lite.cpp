6a57fac0ed547234ad0d8876d08c734465244d9b
Jack Meng <jackmeng0814@gmail.com>
2022-03-27 23:33:09 -0500

    Create Template_lite.cpp

---
+++ Template_lite.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <stdio.h>
+#include <algorithm>
+#include <unistd.h>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+  // EXOAD
+}
