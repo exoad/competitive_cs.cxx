5df8a7597286913443348fd2bb8a78583a321815
Jack Meng <jackmeng0814@gmail.com>
2022-07-15 21:25:58 -0400

    Create Template_FINAL.cc

---
+++ Template_FINAL.cc
@@ -0,0 +1,39 @@
+```cpp
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <utility>
+#include <stack>
+#include <bitset>
+
+// polluting the global namespace >:)
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define db(n) std::cout<<"VAR:"<<#n<<"@"<<&n<<"="<<n<<std::endl;
+#define pb push_back
+#define ft front()
+#define fi first
+#define se second
+#define mp make_pair
+#define F0R(i,a,b) for(int i=(a);i<(B);i++)
+#define RA(a,x) for(auto&a:x)
+#define si(x) int((x).size())
+#define xll(n) n.rbegin(),x.rend()
+#define it insert
+#define eb emplace_back
+constexpr int __b(int r){return 1<<r;}
+constexpr int b__(int r){return __b(r)-1;}
+const int MOD = 1e9+7;const int IE = 1e+9;using pi=std::pair<int,int>;using ll=long long;using vi=std:;vector<int>;using vl=std::vector<ll>;using str=std::string;int main(void){
+
+    /// shitty code goes here
+
+
+return 0;}
+```
